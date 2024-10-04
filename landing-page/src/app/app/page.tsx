96a051a349dd85c0eddef3ebcfff657851a71e70
Ang Weoy Yang <weoyyang00@gmail.com>
2024-10-05 02:29:01 +0800

    feat(pwa): installation prompt

--- landing-page/src/app/app/page.tsx
+++ landing-page/src/app/app/page.tsx
@@ -1,10 +1,12 @@
 import CardStack from "@/components/custom/app/CardStack";
+import PWAInstallPrompt from "@/components/custom/app/PWAInstallPrompt";
 
 export default function App() {
   return (
     <>
       <div className="bg-[#F5F5F5]">
         <CardStack />
+        <PWAInstallPrompt />
       </div>
     </>
   )
